dde37284f5f113eafe08acfa4d4e17436d355bdc
Xiao Yu Ren <archerreborn2@gmail.com>
2015-09-28 17:53:48 -0600

    new files tweetlist and tweetlisttest

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -14,6 +14,7 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
 
+
     public void testStart() throws Exception {
         Activity activity = getActivity();
 
