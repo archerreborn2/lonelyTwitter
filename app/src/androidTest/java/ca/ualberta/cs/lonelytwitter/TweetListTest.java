56e18528699f9b8de7b816c849c3c510ac5f5b69
Xiao Yu Ren <archerreborn2@gmail.com>
2015-09-28 18:55:26 -0600

    added new functions to tweetlist

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,6 +1,8 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by dren on 9/28/15.
@@ -16,7 +18,7 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         list.add(new NormalTweet("test"));
     }
 
-    public void testdDelete(){
+    public void testRemoveTweet(){
         TweetList list = new TweetList();
         Tweet tweet = new NormalTweet("test");
         list.add(tweet);
@@ -30,5 +32,25 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         Tweet tweet = new NormalTweet("test");
         list.add(tweet);
         assertTrue(list.contains(tweet));
+    }
+
+    public void testgetCount(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        assertTrue(list.getCount() == 1);
+
+    }
+
+    public void testgetTweet(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test",new Date(2000, 10, 20));
+        Tweet tweet2 = new NormalTweet("test2", new Date(1998, 9, 15));
+        list.add(tweet2);
+        list.add(tweet);
+
+        list.getTweets();
+
+
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,6 +1,7 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by dren on 9/28/15.
@@ -9,10 +10,13 @@ public class TweetList {
     private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 
     public void setUp(){
-        
+
     }
 
     public void add(Tweet tweet){
+        if(contains(tweet)){
+            throw new IllegalArgumentException();
+        }
         tweets.add(tweet);
 
     }
@@ -27,7 +31,35 @@ public class TweetList {
        return tweets.contains(tweet);
     }
 
+    public int getCount(){
+        int i = 0;
+        while(i < tweets.size()){
+            i++;
+        }
 
+        return i;
+    }
+
+    public ArrayList<Tweet> getTweets(){
+        ArrayList<Tweet> sorted = new ArrayList<Tweet>();
+            while(tweets.isEmpty() == false){
+                Tweet tweet = tweets.get(0);
+                Date date = tweet.getDate();
+                for(int i = 0; i < tweets.size(); i++){
+                    Tweet tweet2 = tweets.get(i);
+                    Date date2 = tweet2.getDate();
+                    if(date.compareTo(date2) < 0){
+                        tweet = tweet2;
+                        date = date2;
+                    }
+                }
+                sorted.add(tweet);
+                tweets.remove(tweet);
+            }
+        tweets = sorted;
+
+        return sorted;
+    }
 
 
 }
