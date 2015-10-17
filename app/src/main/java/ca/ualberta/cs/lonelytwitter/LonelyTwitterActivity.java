bb4a5134f13803061dd4bc5557a3a2f901237ef8
Xiao Yu Ren <archerreborn2@gmail.com>
2015-10-16 18:56:14 -0600

    added some code

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -15,8 +15,10 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,11 +30,29 @@ import com.google.gson.reflect.TypeToken;
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav"; // model
+
+	private LonelyTwitterActivity activity = this;
 	private EditText bodyText;
 	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 	private ListView oldTweetsList;
 	private ArrayAdapter<Tweet> adapter;
+	private Button saveButton;
+	private editedTweet toEditTweet = editedTweet.getInstance();
 
+	public Button getSaveButton() {
+		return saveButton;
+	}
+
+	public EditText getBodyText() {
+		return bodyText;
+	}
+
+	public ArrayList<Tweet> getTweets() {
+		return tweets;
+	}
+	public ListView getOldTweetsList() {
+		return oldTweetsList;
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -42,8 +62,16 @@ public class LonelyTwitterActivity extends Activity {
 		setContentView(R.layout.main);
 
 		bodyText = (EditText) findViewById(R.id.body);
-		Button saveButton = (Button) findViewById(R.id.save);
+		saveButton = (Button) findViewById(R.id.save);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+		oldTweetsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				toEditTweet.setTweet((Tweet) oldTweetsList.getItemAtPosition(position));
+				Intent intent = new Intent(activity, EditTweetActivity.class);//dont use "this" because it's referring to the onItemClick
+				startActivity(intent);
+			}
+		});
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
